93aa2285a70e5b58039c74496328bf2a8d866997
GabrielArSouza <gabriel_feg@hotmail.com>
2018-08-28 18:13:06 -0300

    add classes Pesquisa e Campo

---
+++ Java/src/Sistema/Campo.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sistema;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ *
+ * @author gabriel
+ * Representa as visitas de campo realizadas
+ */
+public final class Campo {
+    /**
+     * Atributos básicos
+     */
+    private Calendar data; //data da visita
+    private ArrayList<String> pesquisador; // pesquisadores que foram no campo
+    private Double gastos;
+    private ArrayList<String> convidado; // convidados para o campo
+
+    /**
+     * Construtor básico
+     * @param data a data que foi realizado o campo
+     * @param pesquisador os pesquisadores que foram ao campo
+     * @param gastos o total de gastos com o campo
+     * @param convidado Os convidados levados ao campo
+     */
+    public Campo ( Calendar data, ArrayList<String> pesquisador, Double gastos,
+            ArrayList<String> convidado)
+    {
+        this.data = data;
+        this.pesquisador = pesquisador;
+        this.gastos = gastos;
+        this.convidado = convidado;
+    }
+    
+    /**
+     * Construtor básico
+     * @param data a data que o campo foi realizado
+     * @param pesquisador um pesquisador que foi ao campo
+     * @param gastos O total de gastos com o campo
+     */
+    public Campo ( Calendar data, String pesquisador, Double gastos )
+    {
+        this.data = data;
+        this.pesquisador = new ArrayList<> ();
+        this.inserirPesquisador(pesquisador);
+        this.gastos = gastos;
+    }
+
+    /**
+     * Adiciona um novo pesquisador a lista de pesquisadores existentes
+     * @param novoPesquisador O novo pesquisador
+     */
+    public void inserirPesquisador ( String novoPesquisador )
+    {   this.getPesquisador().add(novoPesquisador); }
+
+    /**
+     * Adiciona um novo convidado a lista de convidados
+     * @param novoConvidado O novo convidado
+     */
+    public void inserirConvidado ( String novoConvidado )
+    {   this.getConvidado().add(novoConvidado); }
+    
+    /**
+     * Incrementa mais um gasto nos gastos totais
+     * @param gasto 
+     */
+    public void incrementaGastos ( Double gasto )
+    {   this.setGastos((Double) (this.getGastos() + gasto)); }
+
+    // Getters and Setters
+    
+    /**
+     * @return the data
+     */
+    public Calendar getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Calendar data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the pesquisador
+     */
+    public ArrayList<String> getPesquisador() {
+        return pesquisador;
+    }
+
+    /**
+     * @param pesquisador the pesquisador to set
+     */
+    public void setPesquisador(ArrayList<String> pesquisador) {
+        this.pesquisador = pesquisador;
+    }
+
+    /**
+     * @return the gastos
+     */
+    public Double getGastos() {
+        return gastos;
+    }
+
+    /**
+     * @param gastos the gastos to set
+     */
+    public void setGastos(Double gastos) {
+        this.gastos = gastos;
+    }
+
+    /**
+     * @return the convidado
+     */
+    public ArrayList<String> getConvidado() {
+        return convidado;
+    }
+
+    /**
+     * @param convidado the convidado to set
+     */
+    public void setConvidado(ArrayList<String> convidado) {
+        this.convidado = convidado;
+    }
+    
+    
+    
+}
---
+++ Java/src/Sistema/Pesquisa.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sistema;
+
+import java.util.ArrayList;
+
+/** 
+ * 
+ * @author gabriel
+ * Representa a classe Pesquisa com os atributos necessários.
+ * Uma pesquisa contém diversos campos realizados
+ */
+public final class Pesquisa {
+    
+    /**
+     * Atributos básicos
+     */
+    private String denomicacao; // Denominação da pesquisa (título)
+    private String sigla; // Sigla da pesquisa (como é mais conhecido)
+    private String financiador; // financiador do projeto (pode virar uma nova classe)
+    private String coordenador; // Coordenador do projeto
+    private ArrayList<String> pesquisador; // Pesquisadores envovidos com a pesquisa
+    
+    private ArrayList<Campo> campos; // Campos realizados na pesquisa
+
+    /**
+     * Construtor básico
+     * @param denominacao o título do projeto
+     * @param sigla a sigla do projeto
+     * @param financiador o financiador da pesquisa
+     * @param coordenador o coordenador da pesquisa
+     * @param pesquisador os pesquisadores 
+     * @param campos os campos já realizados
+     */
+    public Pesquisa (String denominacao, String sigla, String financiador, 
+            String coordenador, ArrayList<String> pesquisador, 
+            ArrayList<Campo> campos)
+    {
+        this.denomicacao = denominacao;
+        this.sigla = sigla;
+        this.financiador = financiador;
+        this.coordenador = coordenador;
+        this.pesquisador = pesquisador;
+        this.campos = campos;
+    }
+    
+    /**
+     * Construtor sem o campo e com inserção de um único pesquisador
+     * @param denominacao o título do projeto
+     * @param sigla a sigla do projeto
+     * @param financiador o financiador da pesquisa
+     * @param coordenador o coordenador da pesquisa
+     * @param pesquisador o pesquisador 
+     */
+    public Pesquisa (String denominacao, String sigla, String financiador, 
+            String coordenador, String pesquisador)
+    {
+        this.denomicacao = denominacao;
+        this.sigla = sigla;
+        this.financiador = financiador;
+        this.coordenador = coordenador;
+        this.pesquisador = new ArrayList<>();
+        this.inserirPesquisador(pesquisador);
+        
+    }    
+    
+    public void inserirPesquisador ( String pesquisador )
+    { this.pesquisador.add(pesquisador); }
+    
+    public void inserirCampo ( Campo campo )
+    { this.campos.add(campo);}        
+    
+    // Getters and Setters
+    
+    /**
+     * @return the denomicacao
+     */
+    public String getDenomicacao() {
+        return denomicacao;
+    }
+
+    /**
+     * @param denomicacao the denomicacao to set
+     */
+    public void setDenomicacao(String denomicacao) {
+        this.denomicacao = denomicacao;
+    }
+
+    /**
+     * @return the sigla
+     */
+    public String getSigla() {
+        return sigla;
+    }
+
+    /**
+     * @param sigla the sigla to set
+     */
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    /**
+     * @return the financiador
+     */
+    public String getFinanciador() {
+        return financiador;
+    }
+
+    /**
+     * @param financiador the financiador to set
+     */
+    public void setFinanciador(String financiador) {
+        this.financiador = financiador;
+    }
+
+    /**
+     * @return the coordenador
+     */
+    public String getCoordenador() {
+        return coordenador;
+    }
+
+    /**
+     * @param coordenador the coordenador to set
+     */
+    public void setCoordenador(String coordenador) {
+        this.coordenador = coordenador;
+    }
+
+    /**
+     * @return the pesquisador
+     */
+    public ArrayList<String> getPesquisador() {
+        return pesquisador;
+    }
+
+    /**
+     * @param pesquisador the pesquisador to set
+     */
+    public void setPesquisador(ArrayList<String> pesquisador) {
+        this.pesquisador = pesquisador;
+    }
+
+    /**
+     * @return the campos
+     */
+    public ArrayList<Campo> getCampos() {
+        return campos;
+    }
+
+    /**
+     * @param campos the campos to set
+     */
+    public void setCampos(ArrayList<Campo> campos) {
+        this.campos = campos;
+    }
+    
+    
+}
