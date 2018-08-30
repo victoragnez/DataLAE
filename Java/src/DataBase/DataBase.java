cc9662679fadf3a949bea497e62c927b5d982e80
Victor <victor.agnez@hotmail.com>
2018-08-29 21:30:15 -0300

    Create DataBase interface

---
+++ Java/src/DataBase/DataBase.java
@@ -0,0 +1,6 @@
+package DataBase;
+
+public interface DataBase {
+	public void insert(Object obj, Object key) throws RepeatedKeyException;
+	public Object get(Object obj, Object key);
+}
---
+++ Java/src/DataBase/RepeatedKeyException.java
@@ -0,0 +1,16 @@
+package DataBase;
+
+public class RepeatedKeyException extends Exception {
+
+	public RepeatedKeyException(){
+		super();
+	}
+
+	public RepeatedKeyException(String mensagem){
+		super(mensagem);
+	}
+
+	public RepeatedKeyException(String mensagem, Throwable causa){
+		super(mensagem, causa);
+	}
+}
