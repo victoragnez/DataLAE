a6e9a2f00e77e86f5c085c2cc02a647447ccacdc
Victor <victor.agnez@hotmail.com>
2018-11-21 11:57:18 -0300

    Add validate method using reflection

--- Framework/src/main/java/framework/service/ServiceArea.java
+++ Framework/src/main/java/framework/service/ServiceArea.java
@@ -1,37 +1,50 @@
 package framework.service;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import framework.dao.interfaces.DatabaseException;
 import framework.dao.interfaces.IDAOArea;
 import framework.model.Area;
+import framework.model.MarcadoresService.ValidarAtualizar;
+import framework.model.MarcadoresService.ValidarConsultar;
+import framework.model.MarcadoresService.ValidarInserir;
+import framework.model.MarcadoresService.ValidarRemover;
 import framework.service.interfaces.IServiceArea;
 
 public abstract class ServiceArea<A extends Area> implements IServiceArea <A>{
 
 	private final IDAOArea<A> dao;
 	
-	public ServiceArea (IDAOArea<A> dao)	{
+	public ServiceArea (IDAOArea<A> dao) {
 		this.dao = dao;
 	}
 	
 	@Override
-	public void inserir(A a) throws DatabaseException{
+	public void inserir(A a) throws DatabaseException {
+		validate(ValidarInserir.class, a);
 		dao.inserir(a);
 	}
 	
 	@Override
-	public void remover(A a) throws DatabaseException{
+	public void remover(A a) throws DatabaseException {
+		validate(ValidarRemover.class, a);
 		dao.remover(a);
 	}
 	
 	@Override
-	public void atualizar(A a) throws DatabaseException{
+	public void atualizar(A a) throws DatabaseException {
+		validate(ValidarAtualizar.class, a);
 		dao.atualizar(a);
 	}
 	
 	@Override
-	public List<A> consultar(A a) throws DatabaseException{
+	public List<A> consultar(A a) throws DatabaseException {
+		validate(ValidarConsultar.class, a);
 		return dao.consultar(a);
 	}
 	
@@ -40,4 +53,21 @@ public abstract class ServiceArea<A extends Area> implements IServiceArea <A>{
 		return dao.listar();
 	}
 	
+	private void validate(Class<? extends Annotation> annotation, A a) {
+		Class<?> curClass = this.getClass();
+	    
+		while (curClass != ServiceArea.class) {
+	        List<Method> allMethods = new ArrayList<Method>(Arrays.asList(curClass.getDeclaredMethods()));       
+	        for (Method method : allMethods) {
+	            if (method.isAnnotationPresent(annotation)) {
+	            	try {
+						method.invoke(this, a);
+					} catch (IllegalAccessException | IllegalArgumentException | 
+							InvocationTargetException e) {
+					}
+	            }
+	        }
+	        curClass = curClass.getSuperclass();
+	    }
+	}
 }
--- Framework/src/main/java/framework/service/ServiceAtividade.java
+++ Framework/src/main/java/framework/service/ServiceAtividade.java
@@ -1,10 +1,19 @@
 package framework.service;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import framework.dao.interfaces.DatabaseException;
 import framework.dao.interfaces.IDAOAtividade;
 import framework.model.Atividade;
+import framework.model.MarcadoresService.ValidarAtualizar;
+import framework.model.MarcadoresService.ValidarConsultar;
+import framework.model.MarcadoresService.ValidarInserir;
+import framework.model.MarcadoresService.ValidarRemover;
 import framework.service.interfaces.IServiceAtividade;
 
 public abstract class ServiceAtividade<A extends Atividade> implements IServiceAtividade<A> {
@@ -17,33 +26,49 @@ public abstract class ServiceAtividade<A extends Atividade> implements IServiceA
 	
 	@Override
 	public void inserir(A a) throws DatabaseException {
+		validate(ValidarInserir.class, a);
 		dao.inserir(a);
 	}
-
+	
 	@Override
 	public void remover(A a) throws DatabaseException {
+		validate(ValidarRemover.class, a);
 		dao.remover(a);
 	}
-
+	
 	@Override
 	public void atualizar(A a) throws DatabaseException {
+		validate(ValidarAtualizar.class, a);
 		dao.atualizar(a);
 	}
-
+	
 	@Override
 	public List<A> consultar(A a) throws DatabaseException {
+		validate(ValidarConsultar.class, a);
 		return dao.consultar(a);
 	}
-
+	
 	@Override
-	public List<A> listar() throws DatabaseException {
+	public List<A> listar() throws DatabaseException{
 		return dao.listar();
 	}
 	
-	/** Metodos que precisam ser implementados */
-	protected abstract void validarInserir(A a);
-	protected abstract void validarRemover(A a);
-	protected abstract void validarAtulizar(A a);
-	protected abstract void validarConsultar(A a);
+	private void validate(Class<? extends Annotation> annotation, A a) {
+		Class<?> curClass = this.getClass();
+	    
+		while (curClass != ServiceArea.class) {
+	        List<Method> allMethods = new ArrayList<Method>(Arrays.asList(curClass.getDeclaredMethods()));       
+	        for (Method method : allMethods) {
+	            if (method.isAnnotationPresent(annotation)) {
+	            	try {
+						method.invoke(this, a);
+					} catch (IllegalAccessException | IllegalArgumentException | 
+							InvocationTargetException e) {
+					}
+	            }
+	        }
+	        curClass = curClass.getSuperclass();
+	    }
+	}
 
 }
