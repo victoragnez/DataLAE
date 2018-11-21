de97ba62bc98d6693bab8e3e4d88f0ff74278937
GabrielArSouza <gabriel_feg@hotmail.com>
2018-11-21 15:34:57 -0300

    corrige erros no model da aplicação geologia

--- Framework/src/main/java/com/lab/data/model/ParticipanteGeologia.java
+++ Framework/src/main/java/com/lab/data/model/ParticipanteGeologia.java
@@ -3,25 +3,41 @@ package com.lab.data.model;
 import framework.model.Participante;
 
 public class ParticipanteGeologia extends Participante{
-
-	private String formacao;
 	
-	public ParticipanteGeologia(String nome, String instituicao, String email, String formacao) {
+	private String cpf;
+	private Categoria categoria;
+	
+	public ParticipanteGeologia(String nome, String instituicao, String email,
+			Categoria categoria) {
 		super(nome, instituicao, email);
-		this.setFormacao(formacao);
+		this.categoria = categoria;
 	}
 	
-	public ParticipanteGeologia(String nome, String instituicao, String email, String formacao, Integer codigo) {
+	public ParticipanteGeologia(String nome, String instituicao, String email,
+			Categoria categoria, String cpf) {
+		super(nome, instituicao, email);
+		this.categoria = categoria;
+		this.cpf = cpf;
+	}
+	
+	public ParticipanteGeologia(String nome, String instituicao, String email,
+			String cpf, Categoria categoria, Integer codigo) {
 		super(nome, instituicao, email, codigo);
-		this.setFormacao(formacao);
+		this.cpf = cpf;
+		this.categoria = categoria;
 	}
-
-	public String getFormacao() {
-		return formacao;
+	
+	public String getCpf() {
+		return cpf;
 	}
-
-	public void setFormacao(String formacao) {
-		this.formacao = formacao;
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
 }
---
+++ Framework/src/main/java/com/lab/data/service/ParticipanteGeologiaService.java
@@ -0,0 +1,39 @@
+package com.lab.data.service;
+
+import com.lab.data.model.ParticipanteGeologia;
+
+import framework.dao.interfaces.IDAOParticipante;
+import framework.service.ServiceParticipante;
+
+public class ParticipanteGeologiaService extends ServiceParticipante<ParticipanteGeologia>{
+
+	public ParticipanteGeologiaService(IDAOParticipante<ParticipanteGeologia> dao) {
+		super(dao);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void validarInserir(ParticipanteGeologia p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void validarRemover(ParticipanteGeologia p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void validarAtualizar(ParticipanteGeologia p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void validarConsultar(ParticipanteGeologia p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
