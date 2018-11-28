8d713e062437941625bf41eb795df005880407c5
Victor <victor.agnez@hotmail.com>
2018-11-28 15:00:14 -0300

    Add AE.model

---
+++ Framework/src/main/java/com/lab/ae/model/ProjetoAE.java
@@ -0,0 +1,21 @@
+package com.lab.ae.model;
+
+import framework.model.Projeto;
+
+public class ProjetoAE extends Projeto<ParticipanteAE> {
+	private String financiador;
+
+	/**
+	 * @return the financiador
+	 */
+	public String getFinanciador() {
+		return financiador;
+	}
+
+	/**
+	 * @param financiador the financiador to set
+	 */
+	public void setFinanciador(String financiador) {
+		this.financiador = financiador;
+	}
+}
