17721d6eee4a14056b09effec72ea48d757a31b5
Victor <victor.agnez@hotmail.com>
2018-11-28 15:16:00 -0300

    Fix attributes for AreaEstatistica

--- Framework/src/main/java/com/lab/estatistica/model/AreaEstatistica.java
+++ Framework/src/main/java/com/lab/estatistica/model/AreaEstatistica.java
@@ -3,20 +3,31 @@ package com.lab.estatistica.model;
 import framework.model.Area;
 
 public class AreaEstatistica extends Area {
-	private String local;
-
+	private String cidade;
+	private String estado;
 	/**
-	 * @return the local
+	 * @return the cidade
 	 */
-	public String getLocal() {
-		return local;
+	public String getCidade() {
+		return cidade;
 	}
-
 	/**
-	 * @param local the local to set
+	 * @param cidade the cidade to set
 	 */
-	public void setLocal(String local) {
-		this.local = local;
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	/**
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
 	}
 	
 }
--- Framework/src/main/java/com/lab/estatistica/service/AreaEstatisticaService.java
+++ Framework/src/main/java/com/lab/estatistica/service/AreaEstatisticaService.java
@@ -3,12 +3,21 @@ package com.lab.estatistica.service;
 import com.lab.estatistica.model.AreaEstatistica;
 
 import framework.dao.interfaces.IDAOArea;
+import framework.model.MarcadoresService.ValidarAtualizar;
+import framework.model.MarcadoresService.ValidarInserir;
 import framework.service.ServiceArea;
 
 public class AreaEstatisticaService extends ServiceArea<AreaEstatistica> {
 
 	public AreaEstatisticaService(IDAOArea<AreaEstatistica> dao) {
 		super(dao);
+	}
+	
+	@ValidarInserir
+	@ValidarAtualizar
+	public void verificaEstado(AreaEstatistica a) {
+		if (a.getEstado() == null)
+			throw new IllegalArgumentException("Estado nulo");
 	}
 	
 }
