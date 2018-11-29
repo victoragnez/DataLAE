4a6f57e8e8f46e09d9331694d675129ed15b04ff
gilneyjr <gilneyjnr@gmail.com>
2018-11-29 07:49:59 -0300

    Criando executável de DataAE

---
+++ Framework/src/main/java/com/lab/ae/DataAEApplication.java
@@ -0,0 +1,12 @@
+package com.lab.ae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAEApplication.class, args);
+	}
+}
